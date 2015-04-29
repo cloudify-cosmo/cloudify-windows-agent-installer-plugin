bb5209adacfe60e4282a88953c7a945897e96760
iliapolo <elip@gigaspaces.com>
2015-04-29 17:55:02 +0300

    CFY-2631 first draft

---
+++ setup.py
@@ -0,0 +1,30 @@
+#########
+# Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+
+from setuptools import setup
+
+setup(
+    name='cloudify-windows-agent-installer-plugin',
+    version='1.2rc1',
+    author='Gigaspaces',
+    author_email='cloudify@gigaspaces.com',
+    packages=['windows_agent_installer'],
+    license='LICENSE',
+    description='Plugin for installing a Cloudify agent on a windows machine',
+    install_requires=[
+        'cloudify-plugins-common==3.2rc1',
+        'pywinrm==0.0.2dev',
+    ]
+)
